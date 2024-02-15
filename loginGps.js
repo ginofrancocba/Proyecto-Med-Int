496fa4beee8941321f4c14e4efb44275b2de9c98
ginofrancocba <ginofrancocba@gmail.com>
2024-02-15 09:33:15 -0300

    Add Change loginGps.js

--- loginGps.js
+++ loginGps.js
@@ -6,9 +6,9 @@ var btnGoogleMaps = document.getElementById('btn-google-maps');
 btnGoogleMaps.addEventListener('click', function() {
 
 // Obtener las coordenadas de la ubicación
-var latitud = -31.40562; // Latitud de la ubicación
+var latitud = -31.412310; // Latitud de la ubicación
   
-var longitud = -64.20990; // Longitud de la ubicación
+var longitud = -64.163600; // Longitud de la ubicación
 
 // Construir la URL de Google Maps con los parámetros de la ubicación  
 var url = 'https://www.google.com/maps?q=' + latitud + ',' + longitud;
